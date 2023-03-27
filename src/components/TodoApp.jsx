f86f814e4c0781a85f970432759788984f73bf17
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-03-27 13:23:21 +0000

    Create Header, InputTodo, TodoItem, TodoList, TodoLogic components

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,9 +1,9 @@
 const TodoApp = () => {
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <>
+      <h1>Hello there!</h1>
       <p>I am in a React Component!</p>
-    </div>
+    </>
   );
 };
 export default TodoApp;
