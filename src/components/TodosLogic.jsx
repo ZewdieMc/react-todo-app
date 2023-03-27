eb8a8fdc6faa50a5af4441210533ed2f25ece8d4
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-03-27 13:52:03 +0000

    Add an array of todo objects in the TodoLogic component

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,4 +1,21 @@
 const TodosLogic = () => {
+  const todos = [
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ];
   return (
     <div>TodosLogic content</div>
   )
