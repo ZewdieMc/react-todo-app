98c4db60b4b92d7dae6cabb41932e4210f30b735
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-03-28 01:36:21 +0000

    Pass onchange handler function as prop

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,10 +1,11 @@
 import TodoItem from 'components/TodoItem';
 
-const TodosList = ({todosProps}) => {
+const TodosList = ({todosProps, handleChange, deleteTodo}) => {
+  
   return (
     <ul>
       {todosProps.map((todo) => (
-        <TodoItem key={todo.id} itemProp={todo.title}/>
+        <TodoItem key={todo.id} itemProp={todo} onChange={handleChange} deleteTodo={deleteTodo}/>
       ))}
     </ul>
   );
