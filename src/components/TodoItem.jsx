a9eb4d9246124c75aea366437d2376dbb4266175
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-03-28 01:37:06 +0000

    call the change handle

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,4 +1,16 @@
-const TodoItem = ({ itemProp }) => {
-  return <li>{itemProp}</li>;
+const TodoItem = ({ itemProp, onChange , deleteTodo}) => {
+
+  return (
+    <li>
+      <input 
+      type="checkbox"
+      checked={itemProp.completed}
+      onChange={()=>onChange(itemProp.id)}/>
+      <button 
+      onClick={()=>deleteTodo(itemProp.id)}
+      >Delete</button>
+      {itemProp.title}
+    </li>
+  );
 };
 export default TodoItem;
