34c682f9c33cb2b8f4aa99b9d592d4a19222d721
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-03-28 08:57:13 +0000

    Add react icon

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,62 @@
-const TodoItem = ({ itemProp, onChange , deleteTodo}) => {
+import styles from 'styles/TodoItem.module.css';
+import { useState } from 'react';
+import { FaTrash } from "react-icons/fa";
+import { AiFillEdit } from "react-icons/ai";
 
+const TodoItem = ({ itemProp, onChange, deleteTodo, setUpdate }) => {
+  const [editing, setEditing] = useState(false);
+
+  const handleEditing = () => {
+    setEditing(true);
+  };
+
+  const handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      setEditing(false);
+    }
+  };
+
+  let viewMode = {};
+  let editMode = {};
+  if (editing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+  
   return (
-    <li>
-      <input 
-      type="checkbox"
-      checked={itemProp.completed}
-      onChange={()=>onChange(itemProp.id)}/>
-      <button 
-      onClick={()=>deleteTodo(itemProp.id)}
-      >Delete</button>
-      {itemProp.title}
+    <li className={styles.item}>
+      <div className={styles.content} style={viewMode}>
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => onChange(itemProp.id)} />
+        <button onClick={handleEditing}>
+          <AiFillEdit style={{ color: "blue", fontSize: "16px" }}/>
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => deleteTodo(itemProp.id)}
+        ><FaTrash style={{ color: "red", fontSize: "16px" }} />
+        </button>
+        <span style={itemProp.completed ? completedStyle : null}>
+          {itemProp.title}
+        </span>
+      </div>
+      <input style={editMode}
+        type="text"
+        value={itemProp.title}
+        className={styles.textInput}
+        onChange={(e) => setUpdate(e.target.value, itemProp.id)}
+        onKeyDown={handleUpdatedDone}
+      />
     </li>
   );
 };
