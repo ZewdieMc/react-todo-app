24e495df2cd5080961a8243e6a457db63bdce59d
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-03-28 08:53:23 +0000

    Add form to the InputTodo component

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,45 @@
-const InputTodo = () => {
+import { useState } from "react";
+import { FaPlusCircle } from "react-icons/fa"
+
+const InputTodo = ({ addTodo }) => {
+  const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(title.trim()){
+      addTodo(title);
+      setMessage('');
+      setTitle('');
+    }
+    else
+      setMessage('please add an item.')
+  }
   return (
-    <form>
-      <input type="text" placeholder="Add Todo..." />
-      <button>Submit</button>
+    <>
+    <form onSubmit={handleSubmit} className='form-container'>
+      <input 
+      type="text" 
+      className="input-text"
+      placeholder="Add Todo..."
+      value={title}
+      onChange={handleChange} />
+      <button className="input-submit">
+         <FaPlusCircle 
+         style={{
+          color: 'green',
+          fontSize: '20px',
+          marginTop: '2px',
+        }}
+         />
+      </button>
     </form>
+      <span className="submit-warning">{message}</span>
+      </>
   );
 };
 export default InputTodo;
