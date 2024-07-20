480a72d5178e1d322115e4c6b0a41ec3fa9b02a5
Zewdie Habtie <zewdiehabtie26@gmail.com>
2024-07-20 15:47:09 +0200

    reverse the todo to make the latest todo appear on top

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -5,7 +5,7 @@ const TodosList = ({
   todosProps, handleChange, deleteTodo, setUpdate,
 }) => (
   <ul>
-    {todosProps.map((todo) => (
+{todosProps.slice().reverse().map((todo) => ( // Use slice() to create a copy before reversing
       <TodoItem
         key={todo.id}
         itemProp={todo}
